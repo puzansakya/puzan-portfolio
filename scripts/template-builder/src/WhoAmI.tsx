ec343577bed2a8d8ae89eef96aed7b1f0c6b84a7
pujan shakya <pujan@rigonepal.com>
2024-11-12 08:24:17 +0545

    Refactor: updates personal info

--- scripts/template-builder/src/WhoAmI.tsx
+++ scripts/template-builder/src/WhoAmI.tsx
@@ -3,7 +3,7 @@ export const WhoAmI = () => {
         <h1 className="font-medium text-2xl underline">Puzan Sakya</h1>
         <p>Full-stack Software Engineer&nbsp;</p>
         <div className="py-4">
-            <p>Maitidevi, Kathmandu | +977-9849799166 | puzansakya@gmail.com | https://puzansakya.com.np</p>
+            <p>Maitidevi, Kathmandu | +977-9849799166 | sakyapuzana@gmail.com | https://puzansakya.com.np</p>
             <a>linkedIn: https://www.linkedin.com/in/pujan-shakya-1213032ba</a>
         </div>
     </>
