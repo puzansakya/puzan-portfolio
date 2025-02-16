9f550a487fdecbb241ade2dae175df48738c65c8
pujan shakya <pujan@rigonepal.com>
2025-02-16 21:57:35 +0545

    updated

--- scripts/template-builder/src/WhoAmI.tsx
+++ scripts/template-builder/src/WhoAmI.tsx
@@ -1,11 +1,15 @@
 export const WhoAmI = () => {
-    return <>
-        <h1 className="font-medium text-2xl underline">Puzan Sakya</h1>
-        <p>Full-stack Software Engineer&nbsp;</p>
-        <div className="py-4">
-            <p>Maitidevi, Kathmandu | +977-9849799166 | sakyapuzana@gmail.com | https://puzansakya.com.np</p>
-            <a>linkedIn: https://www.linkedin.com/in/pujan-shakya-1213032ba</a>
-        </div>
+  return (
+    <>
+      <h1 className="font-medium text-2xl underline">Puzan Sakya</h1>
+      <p>Full-stack Software Engineer&nbsp;</p>
+      <div className="py-4">
+        <p>
+          Maitidevi, Kathmandu | +977-9849799166 | puzansakya@gmail.com |
+          https://puzansakya.com.np
+        </p>
+        <a>linkedIn: https://www.linkedin.com/in/pujan-shakya-1213032ba</a>
+      </div>
     </>
-
-}
+  );
+};
